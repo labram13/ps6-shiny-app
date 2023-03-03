3de70a31f26bdb141345f46ddd55ef43282ea9ab
labram13 <labram@uw.edu>
2023-03-03 00:40:54 -0800

    finished program

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# ps6-shiny-app
+Overview:
+
+In this program, we analyze a data set collected from 2020-2021. The
+data set contains information on loans taken out from students from all over
+the U.S. It's structured to organize the data by school. In addition, we are able
+to look at the different types of loans taken out and how many of those loans
+were disbursed to students. There's plenty to look at in this data set. However,
+I've only manipulated the data to find the average loans by state from all the 
+colleges within those states. So we are able to see the highest to lowest
+average loans per state. 
+
+In terms of how the shiny app works, it is subset into three different panels.
+The first panel is more so general information of the data with the amount of
+data there is and how many columns there are. A random sample of the data
+is also shown to get a better feel for what is present.
+
+Our manipulated data is show in the second tab called plot. Here, we are able
+to select the states to show there average loans taken out by students in
+the form of a bar graph. The user is able to select more than one so they can 
+compare and contrast many different states which is one of the widgets available. 
+In terms of visuals, I've given the user the ability to choose different color 
+palettes to fill the the bar graphs with. Color palettes were installed from the
+package "viridis" so be sure to install it with install.packages("viridis").
+
+In the third and final tab, we see a more uniform data table for the user
+to visualize on that note. Only widget here is the user is able to organize
+the data by highest average loan to lowest by selecting the one option. Table
+is also intractable so they can find a certain state if need be!
+
+
--- app.R
+++ app.R
@@ -1,15 +1,18 @@
 library(tidyverse)
 library(shiny)
+library(forcats)
+library(viridis)
 
-
-uah <- read_delim("Student Loan Debt by School 2020-2021.csv")
+student_loans <- read_delim("Student Loan Debt by School 2020-2021.csv")
 
 ui <- fluidPage(
   titlePanel("PS06 Problem Set"), 
   tabsetPanel(type = "tabs",
               tabPanel("General Information",
                        sidebarLayout(
-                         sidebarPanel (strong("Data Description:"),
+                         sidebarPanel (width = "auto",
+                                       h3(strong("Data Description:")),
+                                       p(),
                                        tags$p(
                                          HTML("In this data set created by <em>Andy Kriebel</em>,
                               we look at loans taken out by students from 
@@ -20,8 +23,8 @@ ui <- fluidPage(
                               all types of data in a data-science environment."),
                                          p(),
                                          tags$p(
-                                           HTML("This dataset contains", nrow(uah), 
-                                           "<strong>rows</strong> and", ncol(uah), 
+                                           HTML("This dataset contains", nrow(student_loans), 
+                                           "<strong>rows</strong> and", ncol(student_loans), 
                                            "<strong>columns</strong>.")),
                                          p("Here is a table of a few sample data
                                            to understand how the dataset is 
@@ -35,20 +38,139 @@ ui <- fluidPage(
                                        )
                          ),
                          mainPanel(
-                           dataTableOutput("sample")
+                           dataTableOutput("intro")
                          )
                        )
               ),
-              tabPanel("Plot")
+              tabPanel("Plot",
+                       sidebarLayout(
+                         sidebarPanel(h3(strong("Average Loans per State:\n")),
+                                      p(),
+                                      p("Here, we have calculated the average
+                                        loans students have taken out (total
+                                        amount divided by number of schools
+                                        per state) in each state. By selecting
+                                        desired states to view, the average
+                                        is arranged from highest to lowest. In
+                                        doing so, you are able to see which states, 
+                                        in comparison, loaned the most amount
+                                        of money to go to school."),
+                                      p(),
+                                      fluidRow(
+                                        column(6,
+                                               radioButtons("color", "Choose color palette!",
+                                                            choices = c("A", 
+                                                                        "B",
+                                                                        "C",
+                                                                        "D"))
+                                        ),
+                                        column(6,
+                                               uiOutput("checkboxState")
+                                        )
+                                      )
+                         ),
+                         mainPanel(
+                           plotOutput("plot"),
+                           textOutput("result")
+                         )
+                       )
+                     ),
+              tabPanel("Table",
+                       sidebarLayout(
+                         sidebarPanel(h3(strong("Table of Averages:\n")),
+                                      p(),
+                                      p("In this table, we manipulated the same
+                                        data to calculate the averages for loans
+                                        taken out per state. The difference here
+                                        is it isn't organized! We've provided
+                                        an option to organize the results from highest
+                                        to lowest just as in the graph page.
+                                        The data table is also interactable so you can 
+                                        search up a certain state if you would like (
+                                        use abbreviations for states).
+                                        )
+                                        "),
+                           uiOutput("checkboxCol")
+                         ),
+                         mainPanel(
+                           dataTableOutput("table")
+                         )
+                       )
+              )
   )
 )
-
-server <- function(input, output) {
-  output$sample <- renderDataTable({
-    uah %>% 
+server <- function(input, output){
+  plt <- student_loans %>% 
+    filter(`Loan Type` == "Total",
+           !is.na(`$ of Disbursements`),
+           !is.na(State)) %>% 
+    group_by(State, School) %>% 
+    summarize(total_loans = max(`$ of Disbursements`)) %>% 
+    group_by(State) %>% 
+    reframe(avg_loan = mean(total_loans))
+  output$intro <- renderDataTable({
+    student_loans %>% 
       head(5)
   })
-  
+  output$ex2 <- DT::renderDataTable(
+    DT::datatable(
+      plt, options = list(
+        lengthMenu = list(c(5, 15, -1), c('5', '15', 'All')),
+        pageLength = 15
+      )
+    )
+  )
+  output$checkboxState <- renderUI({
+    selectizeInput(
+      "State", strong("Select State or Type State abbr. e.g. WA"),
+      choices =  unique(student_loans$State),
+      multiple = TRUE
+    )
+  })
+  sample <- reactive({
+    plt %>% 
+      filter(State %in% input$State)
+  })
+  output$plot <- renderPlot ({
+      ggplot(data = sample()) +
+      geom_col(aes(x = forcats::fct_reorder(State, desc(avg_loan)), 
+                   y = avg_loan, fill = State ),
+               labels =scales::comma
+               ) +
+      scale_y_continuous(labels = scales::comma) +
+      theme(legend.position="none")+
+      labs(title = "Average Loans Taken Out By Students per State",
+           x = "State",
+           y = "$ of Average Loans") +
+      scale_fill_viridis(discrete = TRUE, option = input$color)
+  })
+  output$result <- renderText({
+    max <- sample() %>% 
+      pull(avg_loan) %>% 
+      max()
+  if(is.infinite(max))
+    paste("Please select some states to start showing showing data.")
+  else
+    ""
+  })
+  loan_table <- reactive({
+    plt  %>% 
+      dplyr::arrange(dplyr::desc(!!rlang::sym(input$choose)))
+  })
+  output$table <- renderDataTable({
+    if (is.null(input$choose)){
+      plt
+    } else {
+      loan_table()
+    }
+  })
+  output$checkboxCol <- renderUI({
+    checkboxGroupInput(
+      "choose", strong("Select to arrange from highest to lowest 
+                       in Average Loans per State!"),
+      choices =  c("avg_loan")
+    )
+  })
 }
 
 shinyApp(ui = ui, server = server)
